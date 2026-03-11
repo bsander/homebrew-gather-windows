f8643775a44daa304b269337cb1493c89cbf104f
Sander <>
2026-03-11 15:50:10 +0100

    Fix release workflow YAML by extracting cask to template file

---
+++ cask-template.rb
@@ -0,0 +1,19 @@
+cask "gather-windows" do
+  version "VERSION_PH"
+  sha256 "SHA256_PH"
+
+  url "URL_PH"
+  name "Gather Windows"
+  desc "Move windows from external displays to a target display"
+  homepage "HOMEPAGE_PH"
+
+  depends_on macos: ">= :sonoma"
+
+  app "Gather Windows.app"
+
+  binary "#{appdir}/Gather Windows.app/Contents/MacOS/Gather Windows", target: "gather-windows"
+
+  zap trash: [
+    "~/Library/Preferences/com.vibed.gather-windows.plist",
+  ]
+end
